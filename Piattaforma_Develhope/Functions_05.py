e3d46e91702565ed160d358803a9e4e43f89d698
Luca Della Sciucca <sciucca_@hotmail.it>
2023-02-19 12:42:50 +0100

    19/02/2023 correct Function5 exe

--- Piattaforma_Develhope/Functions_05.py
+++ Piattaforma_Develhope/Functions_05.py
@@ -1,9 +1,9 @@
 import random
 
 def random_list_summer(length = 15):
-    ls = []
-    while len(ls) < length:
-        ls.append(random.randint(-100, 100))
-    print(f"The random list is {ls} and the sum of its values is {sum(ls)}")
+    ls_num = []
+    while len(ls_num) < length:
+        ls_num.append(random.randint(-100, 100))
+    print(f"The random list is {ls_num} and the sum of its values is {sum(ls_num)}")
 
 random_list_summer(40)
